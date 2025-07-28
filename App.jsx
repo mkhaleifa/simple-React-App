fa1530e264d5527861c1f552ae0be88166ba9708
mkhaleifa <sorourmohamed831@gmail.com>
2025-07-28 23:42:32 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import Main from "./components/Main"
+import Header from "./components/Header.jsx"
+import Footer from "./components/Footer.jsx"
+
+
+export default function App() {
+    return (
+        <>
+            <Header />
+            <Main />
+            <Footer />
+        </>
+    )
+}
